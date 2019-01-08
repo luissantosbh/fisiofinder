a84b470a26fccdee69f23293e49afc8c4216c6c7
Luís Santos <luissantosbh@gmail.com>
2019-01-07 22:04:40 -0200

    Trying to run simple queries.

--- fisiofinder/ViewControllerFirestone.swift
+++ fisiofinder/ViewControllerFirestone.swift
@@ -11,32 +11,42 @@ import Firestore
 import Firebase
 
 class ViewControllerFirestone: UIViewController {
-   
-   
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-   
-       simpleQueries()
-
-
+        simpleQueries()
     }
-
+    
     private func simpleQueries() {
         
-         let db = Firestore.firestore()
-     
-        
-        // Criar uma referência para a coleção "cities"
+        let db = Firestore.firestore()
         let citiesRef = db.collection("cities")
+        let myQuery = citiesRef.whereField("state", isEqualTo: "CA")
         
-        // Fazer uma consulta usando a coleção criada anteriormente
-        let consulta = citiesRef.whereField("state", isEqualTo: "CA")
+        myQuery.getDocuments { (snapshot, error) in
+            
+            guard let snapshot = snapshot,
+                error == nil else { // error
+                    return print("database error: \(error!)")
+            }
+            
+            guard !snapshot.isEmpty else { // no data
+                return print("no data")
+            }
+            
+            // data fetched, loop through documents
+            
+            for doc in snapshot.documents {
+                
+                guard let stateProperty = doc.get("state") as? String else {
+                    continue // continue loop
+                }
+                print(stateProperty)
+                
+            }
+            
+        }
         
-        
-        print("Retorno da minha consulta: \(consulta)")
-    
     }
+    
 }
